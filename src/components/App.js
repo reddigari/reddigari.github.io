2f3bbd970439699d3e6ed0f78fe02737dffe3278
Samir Reddigari <samirreddigari@gmail.com>
2020-01-13 23:50:01 -0500

    refactor: render menu on root route

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,29 @@
 import React from 'react';
+import { Container, Col, Row } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Header from './Header'
 import Sidebar from './Sidebar'
-import MainMenuLink from './MainMenuLink'
-import { Container, Col, Row } from 'react-bootstrap';
+import MainMenu from './MainMenu'
 import '../styles/App.css';
 
 function App() {
   return (
-    <Container className="App">
-        <Row className="justify-content-center">
-            <Sidebar imagePath="/sidebar-photo.png"
-                blurbText="Developer, neuroscientist, and data-lover." />
-            <Col md={6}>
-                <Header title="Samir Reddigari" />
-                <Row className="mt-3">
-                    <MainMenuLink path="about" text="About Me" color="#880000" />
-                    <MainMenuLink path="research" text="Research" color="#008800" />
-                    <MainMenuLink path="projects" text="Projects" color="#000088" />
-                    <MainMenuLink path="resume" text="Resume" color="#880088" />
-                </Row>
-            </Col>
-        </Row>
-    </Container>
+    <Router>
+        <Container className="App">
+            <Row className="justify-content-center">
+                <Sidebar imagePath="/sidebar-photo.png"
+                    blurbText="Developer, neuroscientist, and data-lover." />
+                <Col md={6}>
+                    <Header title="Samir Reddigari" />
+                    <Row className="mt-3">
+                        <Switch>
+                            <Route path="/" exact component={MainMenu} />
+                        </Switch>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
+    </Router>
   );
 }
 
---
+++ src/components/MainMenu.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import '../styles/MainMenu.css';
+
+
+function MainMenu(props) {
+    return (
+        <Row>
+            <MainMenuLink path="about" text="About Me" color="#880000" />
+            <MainMenuLink path="research" text="Research" color="#008800" />
+            <MainMenuLink path="projects" text="Projects" color="#000088" />
+            <MainMenuLink path="resume" text="Resume" color="#880088" />
+        </Row>
+    )
+}
+
+function MainMenuLink(props) {
+
+
+    const linkStyle = {
+        borderLeft: `15px solid ${props.color}`
+    }
+
+    return (
+        <Col md={6}>
+            <Link className="MainMenuLink" to={props.path} 
+                style={linkStyle}>
+                {props.text}
+            </Link>
+        </Col>
+    )
+}
+
+export default MainMenu;
