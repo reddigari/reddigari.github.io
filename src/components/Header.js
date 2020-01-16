6a4fb38cf787bca36f42228374a0cf03f5040317
Samir Reddigari <samirreddigari@gmail.com>
2020-01-15 22:11:28 -0500

    feat: show header nav when not on home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { withRouter } from 'react-router-dom';
+import NavMenu from './NavMenu';
 import '../styles/Header.css';
 
 function Header(props) {
+    const { location } = props;
     return (
         <Row className="Header">
-            <Col>
+            <Col md={12}>
                 <h1>{props.title}</h1>
+            </Col>
+            <Col md={12}>
+                {location.pathname !== "/" ? <NavMenu /> : null}
             </Col>
         </Row>
     )
 }
 
-export default Header;
+export default withRouter(Header);
