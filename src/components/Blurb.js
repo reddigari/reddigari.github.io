e53c98cb55059d5a530f9cd5b974a77ce3d9f77d
Samir Reddigari <samirreddigari@gmail.com>
2020-01-17 16:08:25 -0500

    fix: correct blurb style

--- src/components/Blurb.js
+++ src/components/Blurb.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 function Blurb(props) {
-    const style = {textAlign: "middle"};
+    const style = {textAlign: "center"};
     return (
         <div className="Blurb" style={style}>
             {props.text}
