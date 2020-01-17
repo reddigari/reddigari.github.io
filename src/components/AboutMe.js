f129e7752a6efaa37b56f7f72ca612658e0c9b8b
Samir Reddigari <samirreddigari@gmail.com>
2020-01-17 15:06:29 -0500

    feat: add AboutMe component (static content)

---
+++ src/components/AboutMe.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
+function AboutMe(props) {
+    return (
+        <Col>
+            <p>I am the lead software developer at <a href="http://brainspecmed.com">BrainSpec</a> in Boston, MA.
+               I'm also pursuing a Masters in software engineering from the Harvard Extension School. 
+            </p>
+
+            <p>Hobby projects I've worked on range from a React app for <a href="https://reddigari.github.io/mbta-tracker">
+               Boston public transit arrival times</a>, to a <a
+               href="https://whatsthatnumber.xyz">serverless digit recognition
+               application</a>, to an interactive visualization of
+               <a href="https://reddigari.github.io/HopViz"> hop flavor and aroma characteristics</a>
+            </p>
+
+            <p>Formerly I was a researcher in the <a href="http://psych.nyu.edu/pylkkanen/lab/">NYU
+               Neurolinguistics Lab</a>,
+               where I used magnetoencephalography to address questions about the neural underpinnings of human
+               language. My interests are in the brain's representation of thematic relations, the predictive
+               mechanisms employed during verb processing, and how linguistic context modulates language
+               processing in general. Check out my research <Link to="/research">here</Link>.
+            </p>
+        </Col>
+    )
+}
+
+export default AboutMe
