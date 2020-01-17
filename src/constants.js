cfe79a4101fd27f5e7c344b16c323ccf9831c47c
Samir Reddigari <samirreddigari@gmail.com>
2020-01-17 16:09:47 -0500

    chore: add constants

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+const DATA_URL="https://s3.amazonaws.com/reddigari.github.io/data.json";
+const PHOTO_URL="https://s3.amazonaws.com/reddigari.github.io/sidebar-photo.png";
+
+export { DATA_URL, PHOTO_URL };
