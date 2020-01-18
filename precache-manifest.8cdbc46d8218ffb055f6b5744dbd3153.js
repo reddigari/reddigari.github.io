9e1bf0a14a686a9d7874d38f2d0c115e42fa897e
Samir Reddigari <samirreddigari@gmail.com>
2020-01-18 12:43:43 -0500

    Updates

---
+++ precache-manifest.8cdbc46d8218ffb055f6b5744dbd3153.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f4ebdc5d15323e2d79f15247a631ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b1f688ea215144fc459",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "457b8691f2de3a10d8a4",
+    "url": "/static/css/main.64857833.chunk.css"
+  },
+  {
+    "revision": "2b1f688ea215144fc459",
+    "url": "/static/js/2.19b4a178.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/static/js/2.19b4a178.chunk.js.LICENSE"
+  },
+  {
+    "revision": "457b8691f2de3a10d8a4",
+    "url": "/static/js/main.df4bb268.chunk.js"
+  },
+  {
+    "revision": "af985be99b1c54b29e4d",
+    "url": "/static/js/runtime-main.8e3e1a2d.js"
+  },
+  {
+    "revision": "eae36ae0bbdfd3f8c856bf59995e1c16",
+    "url": "/static/media/cmunss.eae36ae0.woff"
+  }
+]);
